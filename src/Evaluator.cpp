ece1f05ab8d5906fc89b4df7ba6af0cd8baee0a5
Alberto Ocaranza <fevil.corp@gmail.com>
2021-10-01 12:22:41 -0500

    Se añadieron algunas operaciones más con vectores 3D

--- src/Evaluator.cpp
+++ src/Evaluator.cpp
@@ -62,21 +62,39 @@ Evaluator::Evaluator(string type, string e): _exp(e){
       this->sumbr_v2D(matches[4], matches[7], matches[13], matches[16], matches[23], matches[26]);
     }
   }else if(aux_match == 5){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      this->sumbl_v2D(matches[3], matches[6], matches[13], matches[16], matches[22], matches[25]);
+    }
   }else if(aux_match == 6){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      this->res_v2D(matches[3], matches[6], matches[12], matches[15]);
+    }
   }else if(aux_match == 7){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      this->resb_v2D(matches[4], matches[7], matches[13], matches[16]);
+    }
   }else if(aux_match == 8){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      cout << types[aux_match];
+      // this->resbr_v2D(matches[4], matches[7], matches[13], matches[16], matches[23], matches[26]);
+    }
   }else if(aux_match == 9){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      cout << types[aux_match];
+      // this->resbl_v2D(matches[3], matches[6], matches[13], matches[16], matches[22], matches[25]);
+    }
   }else if(aux_match == 10){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      this->sum_v3D(matches[3], matches[6], matches[9], matches[15], matches[18], matches[21]);
+    }
   }else if(aux_match == 11){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      this->sumb_v3D(matches[4], matches[7], matches[10], matches[16], matches[19], matches[20]);
+    }
   }else if(aux_match == 12){
-
+    if(regex_search(_exp, matches, comparisson[aux_match])){
+      cout << types[aux_match] << endl;
+    }
   }else if(aux_match == 13){
 
   }else if(aux_match == 14){
@@ -89,6 +107,8 @@ Evaluator::Evaluator(string type, string e): _exp(e){
 
   }else if(aux_match == 18){
 
+  }else{
+    cout << "Invalid expression: " << _exp << endl;
   }
   
 }
@@ -106,6 +126,33 @@ void Evaluator::sumb_v2D(string x, string y, string x1, string y1){
 }
 void Evaluator::sumbr_v2D(string x, string y, string x1, string y1, string x2, string y2){
   float v1_x = stof(x) + stof(x1);
-  float v1_y = stof(y) + stof(y);
+  float v1_y = stof(y) + stof(y1);
   cout << "R => (" << v1_x + stof(x2) << "," << v1_y + stof(y2) << ")" << endl; 
+}
+void Evaluator::sumbl_v2D(string x, string y, string x1, string y1, string x2, string y2){
+  float v1_x = stof(x) + stof(x1);
+  float v1_y = stof(y) + stof(y1);
+  cout << "R => (" << v1_x + stof(x2) << "," << v1_y + stof(y2) << ")" << endl; 
+}
+void Evaluator::res_v2D(string x, string y, string x1, string y1){
+  cout << "R => (" << stof(x) - stof(x1) << "," << stof(y) - stof(y1) << ")" << endl; 
+}
+void Evaluator::resb_v2D(string x, string y, string x1, string y1){
+  cout << "R => (" << stof(x) - stof(x1) << "," << stof(y) - stof(y1) << ")" << endl; 
+}
+void Evaluator::resbr_v2D(string x, string y, string x1, string y1, string x2, string y2){
+  float v1_x = stof(x) - stof(x1);
+  float v1_y = stof(y) - stof(y1);
+  cout << "R => (" << v1_x - stof(x2) << "," << v1_y - stof(y2) << ")" << endl; 
+}
+void Evaluator::resbl_v2D(string x, string y, string x1, string y1, string x2, string y2){
+  float v1_x = stof(x) + stof(x1);
+  float v1_y = stof(y) + stof(y1);
+  cout << "R => (" << v1_x + stof(x2) << "," << v1_y + stof(y2) << ")" << endl; 
+}
+void Evaluator::sum_v3D(string x, string y, string z, string x1, string y1, string z1){
+  cout << "R => (" << stof(x)+stof(x1) <<","<<stof(y)+stof(y1)<<","<<stof(z)+stof(z1)<<")"<<endl;
+}
+void Evaluator::sumb_v3D(string x, string y, string z, string x1, string y1, string z1){
+  cout << "R => (" << stof(x)+stof(x1) <<","<<stof(y)+stof(y1)<<","<<stof(z)+stof(z1)<<")"<<endl;
 }
--- src/Evaluator.hpp
+++ src/Evaluator.hpp
@@ -14,5 +14,13 @@ class Evaluator{
   void sum_v2D(string x, string y, string x1, string y1);
   void sumb_v2D(string x, string y, string x1, string y1);
   void sumbr_v2D(string x, string y, string x1, string y1, string x2, string y2);
+  void sumbl_v2D(string x, string y, string x1, string y1, string x2, string y2);
+  void res_v2D(string x, string y, string x1, string y1);
+  void resb_v2D(string x, string y, string x1, string y1);
+  void resbr_v2D(string x, string y, string x1, string y1, string x2, string y2);
+  void resbl_v2D(string x, string y, string x1, string y1, string x2, string y2);
+  void sum_v3D(string x, string y, string z, string x1, string y1, string z1);
+  void sumb_v3D(string x, string y, string z, string x1, string y1, string z1);
 };
+
 #endif
